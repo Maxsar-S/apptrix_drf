fa610315b0b38f8ac8439180cd5c3e48cc77a1a6
ALex <alekseibarannikov@mail.ru>
2022-02-17 20:47:26 +0300

    Создал модель участников. У участника есть аватарка, пол, имя и фамилия и почта.

---
+++ apptrix/connect/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    MALE = 'M'
+    FEMALE = 'W'
+
+    GENDER_CHOISES = (
+        (MALE, 'M'),
+        (FEMALE, 'W'),
+    )
+
+    email = models.EmailField(unique=True)
+    gender = models.CharField(choices=GENDER_CHOISES, max_length=1, verbose_name='gender')
+    user_image = models.ImageField(upload_to='users_images', blank=True)
